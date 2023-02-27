f35ffebf9caccb0b7f4748260b440f916383dfde
Tatevik Mkrtchyan <mkrtchyantatev@gmail.com>
2023-02-27 22:45:26 +0400

    With this change the script writes ra and dec of the object as well as saving it fits the format of ".fits".

--- sed_calculation.py
+++ sed_calculation.py
@@ -2,13 +2,14 @@ import shutil
 
 import numpy as np
 import os
-from astropy.io import ascii
+from astropy.io import ascii, fits
+from astropy.table import Table
 
 
 
 # ------------------------------- Define functions ---------------------------------
 
-# function for a_scale which is a scaling factor and takes as an input of observed flux, observed flux error of the object and the template model
+# function for a_scale which is a scaling factor and takes as an input of observed flux, observed flux error of the source and the template model
 def a_scale(vec_flux_obs, vec_fluxe_obs, vec_flux_model):
     # ---- Obtain scaling factor for Chi2
     a = np.sum((vec_flux_obs * vec_flux_model) / (vec_fluxe_obs) ** 2) / np.sum(
@@ -16,7 +17,7 @@ def a_scale(vec_flux_obs, vec_fluxe_obs, vec_flux_model):
     return a
 
 
-# function for calculation of chi2 statistical parameter and takes as an input of observed flux, observed flux error of the object and the template model and a scaling factor
+# function for calculation of chi2 statistical parameter and takes as an input of observed flux, observed flux error of the source and the template model and a scaling factor
 def chi2_calc(vec_flux_obs, vec_fluxe_obs, vec_flux_model, a):
     # ---- Obtain scaling factor for Chi2
     chi2 = np.sum((vec_flux_obs - a * vec_flux_model) ** 2 / (vec_fluxe_obs) ** 2)
@@ -32,7 +33,7 @@ def flux_from_cgs_to_mJy(flux_cgs, ll):
 
 # ------------------------------- Main code ---------------------------------
 # I: Define the path and print the data in the created directory
-objects_path = os.path.abspath('input_test/test_input_flux.dat.dat')
+obj_path = os.path.abspath('input_test/input_stripe82_ARAA.dat')
 BD_temp_path = os.path.abspath('input_test/BDRA_fluxes_mJy_202301.dat')
 QSO_temp_path = os.path.abspath('input_test/Selsing+Matt_temp.dat')
 QSO_spec_path = os.path.abspath('input_test/Selsing2015.dat')
@@ -40,8 +41,8 @@ output_folder = os.path.join(os.path.abspath(''), 'output_test')
 if os.path.exists(output_folder):
     shutil.rmtree(output_folder)
 os.mkdir(output_folder)
-output_file = os.path.abspath('output_test/results.csv')
 
+# ----------------- Printing and reading files -----------------
 print("READING BD:", BD_temp_path, "FILE")
 data_bd_temp = ascii.read(BD_temp_path)
 # print(data_bd_temp)
@@ -50,17 +51,13 @@ print("READING QSOs:", QSO_temp_path, "FILE")
 data_qso_temp = ascii.read(QSO_temp_path)
 # print(data_qso_temp)
 
-print("READING INPUT CATALOG:", objects_path, "FILE")
-data_obj = ascii.read(objects_path)
+print("READING INPUT CATALOG:", obj_path, "FILE")
+data_obj = ascii.read(obj_path)
 # print(data_obj[0])
 
 print("READING SELSING 2015:", QSO_spec_path, "FILE")
 data_qso_spec = ascii.read(QSO_spec_path)
 # print(data_qso_spec)
-
-# Copying input file to write new calculations in it
-output_table = []
-
 
 # IIa : Make the vector arrays for Brown Dwarf templates
 BD_g_decam = data_bd_temp.columns[1]
@@ -74,15 +71,14 @@ BD_K_vhs = data_bd_temp.columns[8]
 BD_W1 = data_bd_temp.columns[9]
 BD_W2 = data_bd_temp.columns[10]
 
-# Read BD template rows in a loop
+# ----------------- Read BD template rows in a loop -----------------
 BD_vec_flux = []
-
 for i in range(len(data_bd_temp)):
     vec_flux = [BD_g_decam[i], BD_r_decam[i], BD_i_decam[i], BD_z_decam[i], BD_Y_vhs[i], BD_J_vhs[i], BD_H_vhs[i],
                 BD_K_vhs[i], BD_W1[i], BD_W2[i]]
     BD_vec_flux.append(vec_flux)
 
-# Make list with BDs type and print first template as an example
+# ----------------- Make list with BDs type and print first template as an example -----------------
 BD_all_vec_flux = np.array(BD_vec_flux)
 BD_type_vec = ["BD1", "BD2", "BD3", "BD4", "MD1", "MD2", "MD3"]
 
@@ -106,9 +102,8 @@ QSO_temp_type = data_qso_temp.columns[0]
 QSO_temp_emline = data_qso_temp.columns[12]
 QSO_temp_ebv = data_qso_temp.columns[13]
 
-# ----- Read Column of Redshift in a loop and print the first template as an example
+# ----------------- Read Column of Redshift in a loop and print the first template as an example -----------------
 QSO_vec_flux = []
-
 for i in range(len(data_qso_temp)):
     vec_flux = [QSO_g_decam[i], QSO_r_decam[i], QSO_i_decam[i], QSO_z_decam[i], QSO_Y_vhs[i], QSO_J_vhs[i],
                 QSO_H_vhs[i], QSO_K_vhs[i], QSO_W1[i], QSO_W2[i]]
@@ -126,8 +121,8 @@ errors = [i for i in header if select_err in i]
 wavebands = [i for i in header if i not in errors]
 print("-----------------------------------------WAVEBANDS ------------------------------------------------------")
 print(wavebands)
-object_name = data_obj.columns[0]
-object_redshift = data_obj["redshift"]
+obj_name = data_obj.columns[0]
+obj_redshift = data_obj["redshift"]
 
 # IVa : Make array for central fluxes of the bands(each of them)
 vec_flux = data_obj[
@@ -143,36 +138,44 @@ vec_fluxe = data_obj[
 # print("------------------------------------------ WAVEBAND ERRORS----------------------------------------------------")
 # print(errors)
 
-# Changing flux errors if they are smaller 10 times than the flux
+# ----------------- Changing flux errors if they are smaller 10 times than the flux -----------------
 for i in range(len(vec_flux.columns)):
     x = vec_flux.columns[i]
     y = vec_fluxe.columns[i]
     for j in range(len(data_obj)):
         if y[j] < 0.1 * x[j]:
             y[j] = 0.1 * x[j]
-        if x[j] <= 10 ** -30:
+        if x[j] <= 10 ** -20:
             x[j] = np.nan
+            y[j] = np.nan
 
-# print("------------------------------------------ FLUX ERRORS----------------------------------------------------")
-# print(vec_fluxe)
-
-# c. Make all arrays with the same dtype in this case float
+# c. ----------------- Make all arrays with the same type in this case float -----------------
 vec_flux_model_BD = BD_all_vec_flux.astype(float)
 vec_flux_model_QSO = QSO_all_vec_flux.astype(float)
+
+# ----------------- Making empty lists to write new calculations in it -----------------
+ls_id_l = []
+BD_min_vec = []
+QSO_min_vec =[]
+ra = data_obj.columns[1]
+dec = data_obj.columns[2]
+BD_Chi2_temp_l = []
+QSO_Chi2_temp_l = []
+QSO_emline_l = []
+QSO_ebv_l =[]
+QSO_temp_t = []
+ratio = []
 
 # V : Calculate Chi2 for BD and QSO templates
 for i in range(len(data_obj)):
 # for i in range(3):
-    print("----------------------------------------- RUN FOR OBJECT NUMBER", i,
-          "----------------------------------------")
+    print("----------------------------------------- RUN FOR OBJECT NUMBER", i, "----------------------------------------")
 
     BD_Chi2_array = []
     QSO_Chi2_array = []
     R_Chi = []
     a_BD_array = []
     a_QSO_array = []
-    ls_id = object_name[i]
-    object_z = object_redshift[i]
 
     vec_flux_row = vec_flux[i]
     vec_fluxe_row = vec_fluxe[i]
@@ -183,7 +186,7 @@ for i in range(len(data_obj)):
         [vec_fluxe_row[0], vec_fluxe_row[1], vec_fluxe_row[2], vec_fluxe_row[3], vec_fluxe_row[4], vec_fluxe_row[5],
          vec_fluxe_row[6], vec_fluxe_row[7], vec_fluxe_row[8], vec_fluxe_row[9]])
 
-    # ---- mask the nan value, which means it will not be used for the calculation
+    # ----------------- mask the nan value, which means it will not be used for the calculation -----------------
     mask_nan = ~np.isnan(vec_flux_obs0)
     vec_flux_obs = vec_flux_obs0[mask_nan]
     vec_fluxe_obs = vec_fluxe_obs0[mask_nan]
@@ -242,17 +245,33 @@ for i in range(len(data_obj)):
     R_Chi.append(R)
     print("-------------------------------------------------")
     print("The Chi2 ratio is", R)
+    print(R_Chi)
 
     # ---- Collect results of the calculation in one array and sort it in descending order of χ2_Ratio
-    output_table.append(
-        [ls_id, object_z, BD_Chi2_min, BD_Chi2_min_temp, QSO_Chi2_min, QSO_Chi2_min_z, R_Chi, temp_type_best, temp_emline_best,
-         temp_ebv_best])
+    ls_id_l.append([obj_name[i]])
+    BD_min_vec.append(BD_Chi2_min)
+    QSO_min_vec.append(QSO_Chi2_min)
+    BD_Chi2_temp_l.append(BD_Chi2_min_temp)
+    QSO_Chi2_temp_l.append(QSO_Chi2_min_z)
+    QSO_ebv_l.append(temp_ebv_best)
+    QSO_emline_l.append(temp_emline_best)
+    QSO_temp_t.append(QSO_temp_type)
+    ratio.append(R)
 
-output_table = np.array(output_table, dtype=object)
-output_table.resize([len(data_obj), 10])
-sorted_output_table = sorted(output_table, key=lambda x:(x[6], x[4]))
-output_headers = ['Object_Name', 'Object_Redshift', 'BD_χ2_min', 'BD_template', 'QSO_χ2_min', 'QSO_template_z', 'χ2_Ratio', 'QSO_template', 'QSO_temp_emline', 'QSO_temp_ebv']
-sorted_output_table = np.vstack([output_headers, sorted_output_table]) # The final matrix is one element bigger
 
-np.savetxt(output_file, sorted_output_table, fmt='%s')
+ls_id_c = fits.Column(name='Object Name', array=ls_id_l, format='A10')
+ra_c = fits.Column(name='ra', array=ra, format='F')
+dec_c = fits.Column(name='dec', array=dec, format='F')
+BD_min_vec_c = fits.Column(name='BD_chi2_min', array=BD_min_vec, format='F')
+QSO_min_vec_c = fits.Column(name='QSO_chi2_min', array=QSO_min_vec, format='F')
+BD_Chi2_temp_c = fits.Column(name='BD chi2 template', array=BD_Chi2_temp_l, format='A10')
+QSO_Chi2_temp_c = fits.Column(name='QSO z', array=QSO_Chi2_temp_l, format='F')
+QSO_temp_type_c = fits.Column(name='QSO temptype', array=QSO_temp_t, format='A10')
+QSO_ebv_c = fits.Column(name='QSO EBV', array=QSO_ebv_l, format='F')
+QSO_emline_c = fits.Column(name='QSO  EMline', array=QSO_emline_l, format='F')
+R_chi_c = fits.Column(name='R_chi2_best', array=ratio, format='F')
+t_out = fits.BinTableHDU.from_columns([ls_id_c, ra_c, dec_c, BD_min_vec_c, BD_Chi2_temp_c, QSO_min_vec_c, QSO_Chi2_temp_c, R_chi_c, QSO_ebv_c, QSO_emline_c])
+name_t_out= os.path.abspath('output_test/results.fits')
+t_out.writeto(name_t_out, overwrite=True)
+
 
